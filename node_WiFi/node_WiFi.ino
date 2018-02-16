e5799753088863be2b0fce070b0af09f5703f8ed
Miron Zelina <mironzelinajr@gmail.com>
2018-02-16 23:02:25 +0000

    Add CC3200 WiFi example code
    Temporary workaround to LoRa.

---
+++ node_WiFi/node_WiFi.ino
@@ -0,0 +1,61 @@
+#ifndef __CC3200R1M1RGC__
+// Do not include SPI for CC3200 LaunchPad
+#include <SPI.h>
+#endif
+#include <WiFi.h>
+
+#define DEBUG 1
+
+int moistureSensorPin = A0;
+int moistureSensorValue = 0;
+
+//WiFi settings
+char ssid[] = "test";
+char password[] = "test";
+
+WiFiClient client;
+
+void setup() {
+  if(DEBUG){
+    Serial.begin(9600);
+    Serial.print("Connecting to WiFi.");
+  }
+  
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED) {
+    if(DEBUG) Serial.print(".");
+    delay(300);
+  }
+
+  if(DEBUG) Serial.println();
+  
+  while (WiFi.localIP() == INADDR_NONE) {
+    if(DEBUG) Serial.print(".");
+    delay(300);
+  }
+
+  if(DEBUG){
+    Serial.print("\nIP Address obtained: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+void loop() {
+  moistureSensorValue = analogRead(moistureSensorPin);
+  if(DEBUG) Serial.println(moistureSensorValue);
+  
+  while(client.available()){
+    if(DEBUG) Serial.write(client.read());
+  }
+  
+  if(!client.connected()){
+    client.stop();
+    client.connect("google.com", 80);
+    client.println("GET /index.html HTTP/1.1");
+    client.println("Host: google.com");
+    client.println("Connection: close");
+    client.println("");
+  }
+
+  delay(10000);
+}
